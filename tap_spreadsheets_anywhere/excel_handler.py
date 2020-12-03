f89030c89d90b571364e67f03705c1376ba2c687
rach-green <rachelgreen.745@gmail.com>
2020-12-03 13:36:52 -0500

    added capability to parse for worksheet name if none is provided

--- tap_spreadsheets_anywhere/excel_handler.py
+++ tap_spreadsheets_anywhere/excel_handler.py
@@ -1,6 +1,8 @@
 import re
 import xlrd
+import singer
 
+LOGGER = singer.get_logger()
 
 def generator_wrapper(reader):
     header_row = None
@@ -28,6 +30,25 @@ def generator_wrapper(reader):
 
 def get_row_iterator(table_spec, file_handle):
     workbook = xlrd.open_workbook(on_demand=True,file_contents=file_handle.read())
-    sheet = workbook.sheet_by_name(table_spec["worksheet_name"])
-
+    if "worksheet_name" in table_spec:
+        sheet = workbook.sheet_by_name(table_spec["worksheet_name"])
+    else:
+        try:
+            sheet_name_list = workbook.sheet_names()
+            #if one sheet
+            if(workbook.nsheets == 1):
+                sheet = workbook.sheet_by_name(sheet_name_list[0])
+            #else picks sheet with most data found determined by number of rows
+            else:
+                sheet_list = workbook.sheets()
+                max_row = 0
+                max_name = ""
+                for i in sheet_list:
+                    if i.nrows > max_row:
+                        max_row = i.nrows
+                        max_name = i.name
+                sheet = workbook.sheet_by_name(max_name)
+        except Exception as e:
+            LOGGER.info(e)
+            sheet = workbook.sheet_by_name(sheet_name_list[0])
     return generator_wrapper(sheet.get_rows())
